d91e03c55be833cd5b66e08c127eac9c13b3e737
翟树杰 <hello_worldzsj@qq.com>
2024-01-23 16:46:00 +0800

    1-23 api优化

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,8 @@
-import axios, { type AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, {
+  type AxiosRequestConfig,
+  AxiosResponse,
+  AxiosInstance,
+} from 'axios'
 import { ElMessage } from 'element-plus'
 import { usePiniaStore } from '@/stores'
 import { API_BASE_URL } from '@/constants'
@@ -8,22 +12,28 @@ import router from '@/router'
 declare module 'axios' {
   export interface AxiosResponse<T = any> extends Promise<T> {
     code: number
+    msg: string
+    data: T
   }
 }
 
-axios.defaults.baseURL = API_BASE_URL
-axios.defaults.timeout = 10 * 1000
-axios.defaults.withCredentials = true
+const customRequest = () => {
+  const service: AxiosInstance = axios.create({
+    timeout: 10 * 1000,
+    baseURL: API_BASE_URL,
+    withCredentials: true,
+  })
+  return service
+}
+
+const service = customRequest()
 const { useUserStore } = usePiniaStore()
 
 // 请求拦截器
-axios.interceptors.request.use(
+service.interceptors.request.use(
   (config: AxiosRequestConfig | any): AxiosRequestConfig | any => {
     const { token } = useUserStore
     if (token) config.headers.token = token
-    // if (config.method === 'post') {
-    //   config.data = { info: EncryptData(JSON.stringify(config.data)) }
-    // }
     return config
   },
   (error: any): any => {
@@ -31,17 +41,13 @@ axios.interceptors.request.use(
   }
 )
 // 响应拦截器
-axios.interceptors.response.use(
+service.interceptors.response.use(
   (response: AxiosResponse | any): AxiosResponse | any => {
-    // if (typeof response.data.data === 'string') {
-    //   response.data.data = DecryptData(response.data.data)
-    //   response.data.data = JSON.parse(response.data.data)
-    // }
     const { data } = response
     const { code, msg } = data
     if (code !== 200) {
       ElMessage.error({ message: msg })
-      return Promise.resolve(data)
+      return Promise.reject(data)
     }
     return Promise.resolve(data)
   },
@@ -51,21 +57,9 @@ axios.interceptors.response.use(
     const res = error
     const code = res && res.code
     const message = res && res.message
-    if (res.response.status !== 200) {
-      ElMessage.error({ message: `${code}, ${message}` })
-    }
+    ElMessage.error({ message: `${code}, ${message}` })
     return Promise.reject(error)
   }
 )
-
-// const http: IHttp = {
-//   request(method, url, data) {
-//     return axios({
-//       method: method,
-//       url: url,
-//       data: data,
-//     })
-//   },
-// }
 
 export default axios
