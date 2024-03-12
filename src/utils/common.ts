6284121c77849670abc0265d04d2b5701fef8966
翟树杰 <hello_worldzsj@qq.com>
2024-03-12 09:28:22 +0800

    3-12 工具方法添加

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -55,4 +55,55 @@ const padZero = (num: number) => {
   return num.toString().padStart(2, '0')
 }
 
-export { generateRandomString, getNowTime, formatDate }
+const objEqualNewOld = (a: any, b: any) => {
+  if (a === b) return true
+
+  if (typeof a !== 'object' || a === null || typeof b !== 'object' || b === null) {
+    return false
+  }
+
+  const keysA = Object.keys(a)
+  const keysB = Object.keys(b)
+
+  if (keysA.length !== keysB.length) return false
+
+  for (let key of keysA) {
+    if (!keysB.includes(key)) return false
+    if (typeof a[key] === 'object' && a[key] !== null) {
+      if (!objEqualNewOld(a[key], b[key])) return false
+    } else if (a[key] !== b[key]) {
+      return false
+    }
+  }
+
+  return true
+}
+const arrayEqual = (arr1: any[], arr2: any[]): boolean => {
+  if (arr1 === arr2) return false
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!objEqualNewOld(arr1[i], arr2[i])) {
+      return false
+    }
+  }
+
+  return true
+}
+
+// 防抖
+let timeout: NodeJS.Timeout | null = null
+const debounce = (func: Function, wait: number = 500) => {
+  if (timeout) {
+    clearTimeout(timeout)
+    timeout = null
+  }
+  timeout = setTimeout(() => {
+    timeout = null
+    func()
+  }, wait)
+}
+
+export { generateRandomString, getNowTime, formatDate, debounce, arrayEqual, objEqualNewOld }
